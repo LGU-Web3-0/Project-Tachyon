a7278025a27e88c28736c3cf18d7a10aff917977
SchrodingerZhu <i@zhuyi.fan>
2022-04-01 16:19:29 +0800

    refinement
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

--- tachyon-frontend/gulpfile.js
+++ tachyon-frontend/gulpfile.js
@@ -1,25 +1,24 @@
 const gulp = require("gulp");
 const webpack = require('webpack-stream');
-var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
-gulp.task("default", function () {
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
+const postcss = require('gulp-postcss')
+const sourcemaps = require('gulp-sourcemaps')
 
-  return tsProject
-    .src()
-    .pipe(tsProject())
-    .js
-    .pipe(gulp.dest(".tmp"))
-    .pipe(webpack( require("./webpack.config.js") ))
-    .pipe(gulp.dest("dist"));
+gulp.task("default", function () {
+    return tsProject
+        .src()
+        .pipe(tsProject())
+        .js
+        .pipe(gulp.dest(".tmp"))
+        .pipe(webpack(require("./webpack.config.js")))
+        .pipe(gulp.dest("dist"));
 });
 
 gulp.task('css', () => {
-  const postcss    = require('gulp-postcss')
-  const sourcemaps = require('gulp-sourcemaps')
-
-  return gulp.src('src/**/*.css')
-      .pipe( sourcemaps.init() )
-      .pipe( postcss() )
-      .pipe( sourcemaps.write('.') )
-      .pipe( gulp.dest('dist') )
+    return gulp.src('src/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'))
 })
--- tachyon-frontend/webpack.config.js
+++ tachyon-frontend/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: "production",
+    devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "tachyon.js",
