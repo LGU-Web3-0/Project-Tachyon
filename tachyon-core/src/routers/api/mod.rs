779cf615bf98ad8ebcd7a15f6c341d404729e8a1
Polo <119010079@link.cuhk.edu.cn>
2022-04-16 18:58:09 +0900

    feat: fix id type for task and add task adding function
    Signed-off-by: Polo <119010079@link.cuhk.edu.cn>

--- tachyon-core/src/routers/api/mod.rs
+++ tachyon-core/src/routers/api/mod.rs
@@ -3,6 +3,7 @@ use actix_web::{web, Scope};
 pub mod hello_world;
 pub mod object;
 pub mod status;
+pub mod task;
 pub mod user;
 
 pub fn routers() -> Scope {
@@ -13,4 +14,5 @@ pub fn routers() -> Scope {
         .route("/user/add", web::post().to(user::add))
         .route("/user/login", web::post().to(user::login))
         .route("/user/logout", web::post().to(user::logout))
+        .route("task/add", web::post().to(task::add_task))
 }
