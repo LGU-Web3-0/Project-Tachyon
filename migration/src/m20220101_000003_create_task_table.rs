a7fa35f1d05253129d1ef28695782bc5710bc2d5
Polo <68648546+CyanPineapple@users.noreply.github.com>
2022-04-17 03:55:00 +0800

    feat: task related functions (#40)
    * fix: fix task table create bug
    
    Signed-off-by: Polo <119010079@link.cuhk.edu.cn>
    
    * fix: fix timeStampWithTimeZone for task table
    
    Signed-off-by: Polo <119010079@link.cuhk.edu.cn>
    
    * feat: add task adding function
    
    Signed-off-by: Polo <119010079@link.cuhk.edu.cn>

--- migration/src/m20220101_000003_create_task_table.rs
+++ migration/src/m20220101_000003_create_task_table.rs
@@ -24,20 +24,21 @@ impl MigrationTrait for Migration {
                             .primary_key(),
                     )
                     .col(
+                        ColumnDef::new(entity::task::Column::Name)
+                            .string()
+                            .not_null(),
+                    )
+                    .col(
                         ColumnDef::new(entity::task::Column::CreateDate)
-                            .timestamp()
+                            .timestamp_with_time_zone()
                             .not_null(),
                     )
                     .col(
                         ColumnDef::new(entity::task::Column::DueDate)
-                            .timestamp()
+                            .timestamp_with_time_zone()
                             .not_null(),
                     )
-                    .col(
-                        ColumnDef::new(entity::task::Column::FinishDate)
-                            .timestamp()
-                            .not_null(),
-                    )
+                    .col(ColumnDef::new(entity::task::Column::FinishDate).timestamp())
                     .if_not_exists()
                     .to_owned(),
             )
