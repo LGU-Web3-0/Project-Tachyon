b215489aba026dd2548794bcc950c5a4f24cfc83
Polo <119010079@link.cuhk.edu.cn>
2022-04-18 00:13:18 +0900

    fix: fix avatar for task page
    Signed-off-by: Polo <119010079@link.cuhk.edu.cn>

--- tachyon-template/src/view/task.rs
+++ tachyon-template/src/view/task.rs
@@ -1,14 +1,21 @@
 use super::{LeftBar, LeftBarItem};
+use crate::view::email_hash;
 use sailfish::TemplateOnce;
 
 pub struct TaskItem {
     pub id: i64,
     pub name: String,
+    pub email_h: String,
 }
 
 impl TaskItem {
-    pub fn new(id: i64, name: String) -> Self {
-        Self { id, name }
+    pub fn new(id: i64, email: String, name: String) -> Self {
+        let email_h = email_hash(&email);
+        Self { 
+            id, 
+            name,
+            email_h,
+        }
     }
 }
 
@@ -16,19 +23,22 @@ impl TaskItem {
 #[template(path = "view/task.stpl")]
 pub struct TaskTemplate {
     pub title: String,
+    pub email_hash: String,
     pub items: Vec<TaskItem>,
     pub prev_page_url: Option<String>,
     pub next_page_url: Option<String>,
 }
 
 impl TaskTemplate {
-    pub fn new<T: AsRef<str>>(
+    pub fn new<T: AsRef<str>, E: AsRef<str>>(
         title: T,
+        email: E,
         items: Vec<TaskItem>,
         page_size: usize,
         prev_page: Option<usize>,
         next_page: Option<usize>,
     ) -> Self {
+        let email_hash = email_hash(email);
         let prev_page_url =
             prev_page.map(|n| format!("/view/task?page_size={}&page_no={}", page_size, n));
         let next_page_url =
@@ -36,6 +46,7 @@ impl TaskTemplate {
 
         Self {
             title: title.as_ref().to_string(),
+            email_hash,
             items,
             prev_page_url,
             next_page_url,
@@ -44,5 +55,5 @@ impl TaskTemplate {
 }
 
 impl LeftBar for TaskTemplate {
-    const ACTIVE_ITEM: LeftBarItem = LeftBarItem::User;
+    const ACTIVE_ITEM: LeftBarItem = LeftBarItem::MyTasks;
 }
