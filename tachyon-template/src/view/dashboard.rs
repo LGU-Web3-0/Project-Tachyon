f514743dcad93efe89fffeffc28145b55f6da6a6
Schrodinger ZHU Yifan <i@zhuyi.fan>
2022-04-18 02:24:05 +0800

    feat: dashboard task list (#52)
    * feat: task list
    
    Signed-off-by: Schrodinger Zhu <i@zhuyi.fan>
    
    * feat: task list
    
    Signed-off-by: Schrodinger Zhu <i@zhuyi.fan>

--- tachyon-template/src/view/dashboard.rs
+++ tachyon-template/src/view/dashboard.rs
@@ -2,19 +2,32 @@ use super::{LeftBar, LeftBarItem};
 use crate::view::email_hash;
 use sailfish::TemplateOnce;
 
+pub struct RelatedTask {
+    pub id: i64,
+    pub name: String,
+    pub finished: bool,
+    pub comments: usize,
+}
+
 #[derive(TemplateOnce)]
 #[template(path = "view/dashboard.stpl")]
 pub struct DashboardTemplate {
     pub title: String,
     pub email_hash: String,
+    pub related_tasks: Vec<RelatedTask>,
 }
 
 impl DashboardTemplate {
-    pub fn new<S: AsRef<str>, E: AsRef<str>>(title: S, email: E) -> Self {
+    pub fn new<S: AsRef<str>, E: AsRef<str>>(
+        title: S,
+        email: E,
+        related_tasks: Vec<RelatedTask>,
+    ) -> Self {
         let email_hash = email_hash(email);
         Self {
             title: title.as_ref().to_string(),
             email_hash,
+            related_tasks,
         }
     }
 }
