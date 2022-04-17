e9fddb89f3139d9535e38eb28897fed76c2c85ac
Polo <119010079@link.cuhk.edu.cn>
2022-04-17 22:16:59 +0900

    feat: add UI for task board
    Signed-off-by: Polo <119010079@link.cuhk.edu.cn>

--- tachyon-core/src/routers/view/task.rs
+++ tachyon-core/src/routers/view/task.rs
@@ -7,26 +7,62 @@ use actix_web::web::Data;
 use actix_web::{HttpResponse, Result};
 use entity::sea_orm::{EntityTrait, PaginatorTrait, QueryFilter, QueryOrder};
 use sea_query::{Expr, Order};
-use tachyon_template::{view::TaskTemplate, view::UserTemplate, AsyncRenderOnce};
+use tachyon_template::{view::TaskTemplate, AsyncRenderOnce};
 
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct TaskRequest {
+    page_no: Option<usize>,
+    page_size: Option<usize>,
+}
+
+fn convert_task_info<I>(task: I) -> Vec<tachyon_template::view::TaskItem>
+where
+    I: Iterator<Item = entity::task::Model>,
+{
+    task.map(|t| tachyon_template::view::TaskItem::new(t.id, t.name))
+        .collect()
+}
 
 pub async fn handler(
-    request: actix_web::web::Query<UserRequest>,
+    request: actix_web::web::Query<TaskRequest>,
     data: Data<State>,
     session: Session,
 ) -> Result<HttpResponse> {
     match session.get::<UserInfo>("user")? {
         None => Err(ErrorUnauthorized("login info not found")),
         Some(user) => {
-             UserTemplate::new(
-                 "MyTasks | Project Tachyon",
+            let page = entity::task::Entity::find();
+            let page_size = request.page_size.unwrap_or(10);
+            let paginator = page
+                .order_by(entity::task::Column::Id, Order::Asc)
+                .paginate(&data.sql_db, page_size);
 
-                 )
-                 .render_response()
-                 .await
+            let items = paginator
+                .fetch_page(request.page_no.unwrap_or(0))
+                .await
+                .map_err(ErrorInternalServerError)?;
+            let num_pages = paginator
+                .num_pages()
+                .await
+                .map_err(ErrorInternalServerError)?;
+            let prev_page = match request.page_no {
+                None | Some(0) => None,
+                Some(no) => Some(no - 1),
+            };
+            let next_page = match request.page_no.unwrap_or(0) {
+                n if n + 1 >= num_pages => None,
+                n => Some(n + 1),
+            };
+            let converted = convert_task_info(items.into_iter());
+            TaskTemplate::new(
+                "MyTasks | Project Tachyon",
+                converted,
+                page_size,
+                prev_page,
+                next_page,
+            )
+            .render_response()
+            .await
         }
     }
-
-
-
 }
--- tachyon-template/src/view/task.rs
+++ tachyon-template/src/view/task.rs
@@ -2,10 +2,46 @@ use super::{LeftBar, LeftBarItem};
 use crate::view::email_hash;
 use sailfish::TemplateOnce;
 
+pub struct TaskItem {
+    pub id: i64,
+    pub name: String,
+}
+
+impl TaskItem {
+    pub fn new(id: i64, name: String) -> Self {
+        Self { id, name }
+    }
+}
+
 #[derive(TemplateOnce)]
 #[template(path = "view/task.stpl")]
 pub struct TaskTemplate {
-    pub name: String,
+    pub title: String,
+    pub items: Vec<TaskItem>,
+    pub prev_page_url: Option<String>,
+    pub next_page_url: Option<String>,
+}
+
+impl TaskTemplate {
+    pub fn new<T: AsRef<str>>(
+        title: T,
+        items: Vec<TaskItem>,
+        page_size: usize,
+        prev_page: Option<usize>,
+        next_page: Option<usize>,
+    ) -> Self {
+        let mut prev_page_url =
+            prev_page.map(|n| format!("/view/task?page_size={}&page_no={}", page_size, n));
+        let mut next_page_url =
+            next_page.map(|n| format!("/view/task?page_size={}&page_no={}", page_size, n));
+
+        Self {
+            title: title.as_ref().to_string(),
+            items,
+            prev_page_url,
+            next_page_url,
+        }
+    }
 }
 
 impl LeftBar for TaskTemplate {
